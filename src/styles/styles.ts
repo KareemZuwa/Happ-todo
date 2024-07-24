1181f62bcaec8893df3e36416cae95261de5b5ea
KarreKult <kareemzuwa@gmail.com>
2024-07-24 20:28:13 +0200

    fixing background container color

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -2,6 +2,19 @@ import styled from "styled-components";
 
 /** WRAPPER CONTAINER STYLE */
 export const Container = styled.div`
+  background: #5d4692;
+  background: linear-gradient(
+    137deg,
+    rgba(239, 136, 187, 1) 0%,
+    rgba(41, 24, 80, 1) 100%
+  );
+  height: 100vh;
   max-width: 1440px;
-  background-color: green;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: space-between;
+  text-align: center;
+  overflow-y: auto;
 `;
