5bc1f1cd5a4a93a482c27fccd8f9eef0f0a1a9ce
KarreKult <kareemzuwa@gmail.com>
2024-07-30 19:20:49 +0200

    restoring main component

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,30 +1,9 @@
-import { CheckboxIcon, DeleteIcon, EditIcon } from "../components/_index";
 import { StyledMain } from "../styles/styles";
-/**Temporära imports */
-import { useState } from "react";
 
 export const Main = () => {
-  const [isChecked, setIsChecked] = useState<boolean>(false);
-  const [isEdit, setIsEdit] = useState<boolean>(false);
-  const [isDelete, setIsDelete] = useState<boolean>(false);
-
-  const handleOnChecked = () => {
-    setIsChecked(!isChecked);
-  };
-
-  const handleOnEdit = () => {
-    setIsEdit(!isEdit);
-  };
-
-  const handleOnDelete = () => {
-    setIsDelete(!isDelete);
-  };
   return (
     <StyledMain>
       <div>Main</div>
-      <CheckboxIcon isChecked={isChecked} onToggle={handleOnChecked} />
-      <EditIcon onEdit={handleOnEdit} />
-      <DeleteIcon onDelete={handleOnDelete} />
     </StyledMain>
   );
 };
