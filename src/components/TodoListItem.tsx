c452ffcf2ff9b24fd21059649b4b0e0b3cbdd5f1
KarreKult <kareemzuwa@gmail.com>
2024-07-31 13:14:09 +0200

    creating todolist item from mock data

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,1 +1,40 @@
-export const TodoListItem = () => <div></div>;
+import { useState } from "react";
+import { Subtitle } from "../styles/styles";
+import { Todo } from "../types/interfaces";
+import { CheckboxIcon, DeleteIcon, EditIcon } from "./_index";
+import styled from "styled-components";
+export interface TodoListItemProps {
+  todo: Todo;
+  index: number;
+}
+
+const Box = styled.div`
+  display: flex;
+  gap: 1.5rem;
+`;
+
+export const TodoListItem = ({ todo, index }: TodoListItemProps) => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const handleChecked = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleEdit = () => console.log("editing");
+
+  const handleDelete = () => console.log("deleting");
+  return (
+    <>
+      <Box>
+        <Subtitle>{index + 1}.</Subtitle>
+        <Subtitle>{todo.title}</Subtitle>
+      </Box>
+
+      <Box>
+        <CheckboxIcon isChecked={isChecked} onToggle={handleChecked} />
+        <EditIcon onEdit={handleEdit} />
+        <DeleteIcon onDelete={handleDelete} />
+      </Box>
+    </>
+  );
+};
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,9 +1,20 @@
-import { StyledMain } from "../styles/styles";
+import { useState } from "react";
+import { mockTodos } from "../assets/mock";
+import { TodoListItem, TodoListView } from "../components/_index";
+import { ItemBox, StyledMain } from "../styles/styles";
+import { Todo } from "../types/interfaces";
 
 export const Main = () => {
+  const [todos] = useState<Todo[]>(mockTodos);
   return (
     <StyledMain>
-      <div>Main</div>
+      <TodoListView>
+        {todos.map((todo, index: number) => (
+          <ItemBox key={todo.id}>
+            <TodoListItem todo={todo} index={index} />
+          </ItemBox>
+        ))}
+      </TodoListView>
     </StyledMain>
   );
 };
