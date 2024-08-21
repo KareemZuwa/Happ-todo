367f92af261024e9f0443e351025664bb7de1475
KarreKult <kareemzuwa@gmail.com>
2024-08-21 13:16:47 +0200

    removing unused types

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -67,28 +67,6 @@ export interface ThemeShadows {
 }
 
 /**
- * Interfaces and states used for defining actions and states within the Context store.
- */
-
-export interface Todo {
-  id: string;
-  title: string;
-  completed: boolean;
-}
-
-export interface State {
-  todos: Todo[];
-  newTodoTitle: string;
-}
-
-export type Action =
-  | { type: "ADD_TODO"; payload: string }
-  | { type: "DELETE_TODO"; payload: string }
-  | { type: "EDIT_TODO"; payload: { id: string; title: string } }
-  | { type: "TOGGLE_TODO"; payload: string }
-  | { type: "SET_NEW_TODO_TITLE"; payload: string };
-
-/**
  * Interfaces used for defining props and states within the components directory.
  */
 
